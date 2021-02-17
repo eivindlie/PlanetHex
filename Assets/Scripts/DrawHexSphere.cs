8dea7251917e8c467fb63624a96701cda3d7a191
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2021-02-17 19:24:03 +0100

    Working example with tiled sphere!

--- Assets/Scripts/DrawHexSphere.cs
+++ Assets/Scripts/DrawHexSphere.cs
@@ -7,42 +7,40 @@ using UnityEngine;
 
 public class DrawHexSphere : MonoBehaviour
 {
-
-    public GameObject SpherePrefab;
     // Start is called before the first frame update
     void Start()
     {
-        var hexasphere = new HexasphereGenerator(1.0f, 5, 1.0f).Generate();
+        var hexasphere = new HexasphereGenerator(1000.0f, 5, 1.0f).Generate();
 
         var verts = new List<Vector3>();
         var tris = new List<int>();
 
-        foreach (var tile in hexasphere.Tiles)
-        {
-            foreach (var point in tile.Boundary)
-            {
-                var sphere = Instantiate(SpherePrefab);
-                sphere.transform.position = point.AsVector();
-            }
-        }
-        
-        /*
         var i = 0;
         foreach (var tile in hexasphere.Tiles)
         {
             verts.AddRange(tile.Boundary.Select(p => p.AsVector()));
-            
-            tris.Add(i); tris.Add(i + 1); tris.Add(i + 2);
-            tris.Add(i + 2); tris.Add(i + 3); tris.Add(i + 4);
-            tris.Add(i + 4); tris.Add(i); tris.Add(i + 2);
+
+            tris.Add(i);
+            tris.Add(i + 1);
+            tris.Add(i + 2);
+
+            tris.Add(i + 2);
+            tris.Add(i + 3);
+            tris.Add(i + 4);
+
+            tris.Add(i + 4);
+            tris.Add(i);
+            tris.Add(i + 2);
             if (tile.Boundary.Count > 5)
             {
-                tris.Add(i + 4); tris.Add(i + 5); tris.Add(i);
+                tris.Add(i + 4);
+                tris.Add(i + 5);
+                tris.Add(i);
             }
-            
+
             i += tile.Boundary.Count;
         }
-        
+
         var mesh = new Mesh()
         {
             vertices = verts.ToArray(),
@@ -50,12 +48,9 @@ public class DrawHexSphere : MonoBehaviour
         };
 
         mesh.RecalculateNormals();
-        GetComponent<MeshFilter>().mesh = mesh;*/
+        GetComponent<MeshFilter>().mesh = mesh;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 }
