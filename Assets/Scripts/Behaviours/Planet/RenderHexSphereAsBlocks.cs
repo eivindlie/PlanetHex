8289aeb422410e9891b18dfcbbc67dc47dbfab67
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2021-02-18 19:29:43 +0100

    Assign materials randomly to blocks from a selection of colors

--- Assets/Scripts/Behaviours/Planet/RenderHexSphereAsBlocks.cs
+++ Assets/Scripts/Behaviours/Planet/RenderHexSphereAsBlocks.cs
@@ -1,27 +1,32 @@
+using System.Collections.Generic;
+
 using Meshes;
 
 using PlanetGeneration;
 
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Behaviours.Planet
 {
     public class RenderHexSphereAsBlocks : MonoBehaviour
     {
-        public Material blockMaterial;
-        public float SphereRadius = 100;
-        public float BlockHeight = 10;
+        public List<Material> blockMaterials;
+        public float sphereRadius = 100;
+        public float blockHeight = 10;
 
         public void Start()
         {
-            var hexSphere = new HexSphereGenerator(SphereRadius, 16).Generate();
+            var hexSphere = new HexSphereGenerator(sphereRadius, 16).Generate();
 
             foreach (var tile in hexSphere.Tiles)
             {
                 for (var i = 0; i < Random.Range(1, 5); i++)
                 {
-                    var block = BlockGameObjectFactory.Create(tile, SphereRadius + i * BlockHeight, blockMaterial,
-                        BlockHeight);
+                    var materialIndex = Random.Range(0, blockMaterials.Count); 
+                    var material = blockMaterials[materialIndex];
+                    var block = BlockGameObjectFactory.Create(tile, sphereRadius + i * blockHeight, material,
+                        blockHeight);
                     block.transform.parent = transform;
                 }
             }
