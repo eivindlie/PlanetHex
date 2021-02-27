f58a6cf541e8613dec1f2702e628702c1b1d1278
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2021-02-27 11:44:02 +0100

    Add extension method for getting enumerable with index

---
+++ Assets/Scripts/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> self) =>
+            self.Select((item, index) => (item, index));
+    }
+}
