71ee14ab7cd943f0f5a8d5776ddd9d7c8cab95b7
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2021-02-27 16:07:59 +0100

    Correct sphere distance calculation by project points onto sphere first

--- Assets/Scripts/MathHelpers/SphereHelpers.cs
+++ Assets/Scripts/MathHelpers/SphereHelpers.cs
@@ -6,7 +6,18 @@ namespace MathHelpers
     {
         public static float DistanceBetweenPoints(Vector3 point1, Vector3 point2, float radius)
         {
-            return radius * Mathf.Acos(Vector3.Dot(point1, point2) / Mathf.Pow(radius, 2));
+            var p1 = ProjectToRadius(point1, radius);
+            var p2 = ProjectToRadius(point2, radius);
+            
+            return radius * Mathf.Acos(Vector3.Dot(p1, p2) / Mathf.Pow(radius, 2));
+        }
+
+        private static Vector3 ProjectToRadius(Vector3 point, float radius)
+        {
+            var mag = point.magnitude;
+            var ratio = radius / mag;
+
+            return point * ratio;
         }
     }
 }
